eb5b8ccf8721cec9271841bef37de8c4d989eceb
robjnz <robjnz@outlook.com>
2020-05-21 22:24:14 +0100

    update date

--- src/components/Home/on.vue
+++ src/components/Home/on.vue
@@ -8,7 +8,7 @@
         <span style="font-size: 1em;">
           <i class="far fa-calendar-alt"></i>
         </span>
-  Thursday 21 May 2020
+  Friday 22 May 2020
       </h3>
       <h1 class="text-red-500  text-2xl">
         <span class="text-pink-300" style="font-size: 1em;">
